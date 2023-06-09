bf90f533a3fdc75d28c55d4d70bb267a45479c3e
Yaroslav Korotaev <yaroslav.korotaev@gmail.com>
2023-06-09 18:15:57 +0300

    fix: fix json type

--- src/basic.ts
+++ src/basic.ts
@@ -1,4 +1,4 @@
 export type JsonValue = JsonPrimitive | JsonObject | JsonArray;
 export type JsonPrimitive = string | number | boolean | null;
-export type JsonObject = { [key in string]?: JsonValue };
+export type JsonObject = { [key: string]: JsonValue };
 export type JsonArray = JsonValue[];
